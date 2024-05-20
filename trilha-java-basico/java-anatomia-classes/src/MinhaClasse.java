951d5acf33d4342d7eaa792af153b3144d28f5b1
domingos <mds.junior87@gmail.com>
2024-05-19 22:04:24 -0300

    variaveis e tipos de variaveis

--- trilha-java-basico/java-anatomia-classes/src/MinhaClasse.java
+++ trilha-java-basico/java-anatomia-classes/src/MinhaClasse.java
@@ -2,8 +2,17 @@ public class MinhaClasse {
 
     public static void main(String[] args) {
         
-        System.out.print ("Olá turma, sejam bem-vindos");
+        String primeiroNome = "Moacir";
+        String segundoNome = "Domingos";
 
+        String nomeCompleto = nomeCompleto(primeiroNome, segundoNome)
+
+        System.out.println(nomeCompleto);
+    }
+
+    public static String nomeCompleto (String primeiroNome, String segundoNome) {
+        return primeiroNome.concat(" ").concat(segundoNome);
+        
     }
     
 }
